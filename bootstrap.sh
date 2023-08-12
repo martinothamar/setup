23def3cee2d3bc478e215a2e9a90cff9c0e740a7
Martin Othamar <martin@othamar.net>
2023-08-12 11:52:50 +0200

    configure perf/sysctl, editorconfig, extensions

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+set -ex
+
+echo "----- Installing Python3 and Ansible -------------"
 sudo apt update
 sudo apt install python3 python3-pip ansible
+echo "--------------------------------------------------"
+echo "----- Installing Ansible collections -------------"
 ansible-galaxy collection install community.general
-
+ansible-galaxy collection install ansible.posix
+echo "--------------------------------------------------"
